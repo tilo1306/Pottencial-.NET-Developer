8d7be6e9098b78492609c5a60b4720320233d1d8
Douglas dos Santos Oliveira <doug1306@gmail.com>
2022-09-26 19:02:49 -0300

    feat: add exercise calculator

---
+++ calculadora/Program.cs
@@ -0,0 +1,22 @@
+﻿using calculadora.Models;
+
+Calculadora cal = new Calculadora();
+
+
+Calculadora calc = new Calculadora();
+
+calc.Somar(10,30);
+
+calc.Subtrair(5,5);
+
+calc.Multiplicar(2,2);
+
+calc.Dividir(5,2);
+
+calc.Potencia(2,2);
+
+calc.Seno(30);
+
+calc.Coseno(30);
+
+calc.Tangente(30);
