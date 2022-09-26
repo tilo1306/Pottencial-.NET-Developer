9e23af5a9bb28a131fecd30ec19b038c395c27cf
Douglas dos Santos Oliveira <doug1306@gmail.com>
2022-09-26 19:11:33 -0300

    feat: add exercise multiplication table

---
+++ multiplication table/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine($"Digite um numero para saber sua tabuada!");
+int value = Convert.ToInt32( Console.ReadLine());
+
+for (int i = 0; i <= 10; i++)
+{
+  Console.WriteLine($"{value} * {i} = {value * i} ");
+}
